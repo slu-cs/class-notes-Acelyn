859d9c840fc36648e0f30bf6a63ae095b3a90c21
Ruoya Ding <rding17@stlawu.edu>
2020-02-03 13:24:20 -0500

    JavaScript functions

---
+++ javascript/functions.js
@@ -0,0 +1,22 @@
+// Function syntax
+
+// Function definition
+const avg = function(a,b){
+  return (a + b) / 2;
+};
+
+// Function call
+console.log(avg5,6));
+
+// Another definition
+const print = function(a,b){
+  console.log(`a=${a}, b=${b}`);
+};
+
+// Undefined value
+const result = print(7,8);
+console.log(result);
+print(9);
+print();
+
+//
