4819ebbfc3033fa3ca9b0cb06b43a19459ff78e7
Ruoya Ding <rding17@stlawu.edu>
2020-02-03 13:34:26 -0500

    JavaScript method and constructor

--- javascript/functions.js
+++ javascript/functions.js
@@ -19,4 +19,34 @@ console.log(result);
 print(9);
 print();
 
-//
+// Object literal with a method
+const rectangle = {
+  width:10,
+  height:20,
+  area: function(){
+    return this.width*this.height;
+  }
+};
+
+//method call
+console.log(rectangle.area());
+
+// Constructor definition
+const Rectangle = function(width, height){
+  this.width = width;
+  this.height = height;
+}
+
+// Shared method
+Rectangle.prototype.area = function(){
+  return this.width* this.height;
+};
+
+// Constructing objects
+const small = new Rectangle(1,2);
+const large = new Rectangle(10,20);
+
+
+// Method calls
+console.log(samll.area());
+console.log(large.area());
