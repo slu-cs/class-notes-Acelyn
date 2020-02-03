8b91b18849d8afbf5d7e75cc5ecff70cf0f851d8
Ruoya Ding <rding17@stlawu.edu>
2020-02-03 13:07:44 -0500

    javascript object literal

--- javascript/objects.js
+++ javascript/objects.js
@@ -3,3 +3,23 @@
 // object literal
 const point = {x:1, y:2};
 console.log(point);
+
+// Accessing properties
+console.log(point.x);
+
+// changing properties
+point.x = 3;
+
+// nested object literal
+const circle = {
+  center: point,
+  radius: 4
+}
+
+// Access subproperties
+console.log(circle.center,x);
+
+// Undefined subproperties
+console.log(circle.color);
+circle.color = 'blue';
+console.log(circle);
