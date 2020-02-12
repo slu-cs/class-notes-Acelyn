a913504b81b9ca5576122891432101a74ed2a0de
Ruoya Ding <rding17@stlawu.edu>
2020-02-12 13:54:19 -0500

    new

--- mango/faculty/query.js
+++ mango/faculty/query.js
@@ -1,4 +1,3 @@
-
 // Query the faculty database
 
 const mongoose = require('mongoose');
@@ -10,63 +9,35 @@ connect(); // To the database
 /*// What documents are in the collection?
 const query = Professor.find();
 query.exec(function(error, professors) {
-if (error) console.error(error.stack);
-console.log(professors);
+  if (error) console.error(error.stack);
+  console.log(professors);
 });*/
 
 const queries = [
 
-// What are names in alphabetical order?
-Professor.find().sort('name'),
+  // What are names in alphabetical order?
+  Professor.find().sort('name'),
 
-// Who started most recently?
-Professor.find().sort('-started').limit(1),
+  // Who started most recently?
+  Professor.find().sort('-started').limit(1),
 
-// Who started in 2003?
-Professor.find().where('started').equals(2003),
+  // Who started in 2003?
+  Professor.find().where('started').equals(2003),
 
-// Who teaches 362?
-Professor.find().where('courses').in(362),
+  // Who teaches 362?
+  Professor.find().where('courses').in(362),
 
-// What are all the ranks?
-Professor.distinct('rank')
+  // What are all the ranks?
+  Professor.distinct('rank')
 ];
 
-// Professor.find().sort('name')
-queries[0].exec(function(error, professors) {
-if (error) console.error(error.stack);
-
-const names = professors.map(p => p.name);
-console.log('Names in order: ', names);
-});
-
-// Professor.find().sort('-started').limit(1)
-queries[1].exec(function(error, professors) {
-if (error) console.error(error.stack);
-
-const names = professors.map(p => p.name);
-console.log('Started most recently: ', names);
-});
-
-// Professor.find().where('started').equals(2003)
-queries[2].exec(function(error, professors) {
-if (error) console.error(error.stack);
-
-const names = professors.map(p => p.name);
-console.log('Started in 2003: ', names);
-});
-
-// Professor.find().where('courses').in(362)
-queries[3].exec(function(error, professors) {
-if (error) console.error(error.stack);
-
-const names = professors.map(p => p.name);
-console.log('Teaches 362: ', names);
-});
-
-// Professor.distinct('rank')
-queries[4].exec(function(error, ranks) {
-if (error) console.error(error.stack);
-
-console.log('Distinct ranks: ', ranks);
-});
+// Run the queries in parallel
+Promise.all(queries)
+  .then(function(results) {
+    console.log('Names in order: ', results[0].map(p => p.name));
+    console.log('Started most recently: ', results[1].map(p => p.name));
+    console.log('Started in 2003: ', results[2].map(p => p.name));
+    console.log('Teaches 362: ', results[3].map(p => p.name));
+    console.log('Distinct ranks: ', results[4]);
+    mongoose.connection.close();
+  }).catch(error => console.error(error.stack));
