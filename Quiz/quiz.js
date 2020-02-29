6777c35fa52029d04cac36ed8dc36f4e31bec02e
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-02-29 17:00:16 -0500

    single line

--- Quiz/quiz.js
+++ Quiz/quiz.js
@@ -58,16 +58,9 @@ const user = readline.createInterface({
 const q1 = 'What is your name?';
 const q2 = 'How are you doing?';
 
-user.question(q1, function(){
-  user.on('line', function(line){
-    console.log('Hello '+ line);
-    });
-    user.question(q2, function(){
-      user.on('line', function(line){
-        console.log('I am also '+ line);
-      });
-  });
-});
+user.question(q1, function(line){
+  //user.on('line', function(line){
+    console.log('Hello '+ line);});
 
 // The user.question method doesn't actually return a promise, so here is a question function that does.
 // Call this question function in part B instead of calling the user.question method.
