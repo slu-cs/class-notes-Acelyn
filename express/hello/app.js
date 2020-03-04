a742a9bd86a9e18ff5026fd2bc39f4ab2aa9b280
Ruoya Acelyn Ding <Acelyn@appledeMacBook-Pro-2.local>
2020-03-04 13:35:35 -0500

    express

--- express/hello/app.js
+++ express/hello/app.js
@@ -5,9 +5,31 @@ const express = require('express');
 const app = express();
 
 // Response to one request,
+// cs-linuxlab-13.stlawu.edu:3000
 app.get('/',function(request, response){
   response.send('Hello Ruoya');
 });
+
+// cs-linuxlab-13.stlawu.edu:3000/foo
+app.get('/foo',function(request, response){
+  response.send('Hello foo');
+});
+
+// cs-linuxlab-13.stlawu.edu:3000/foo/bar
+app.get('/foo.bar',function(request, response){
+  response.send('Hello bar');
+});
+
+// cs-linuxlab-13.stlawu.edu:3000/zap/z(for any z)
+app.get('/zap/:z',function(request, response){
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-13.stlawu.edu:3000/zap?x=X&y=Y
+app.get('/zap',function(request, response){
+  response.send(`Hello zap with ${request.query.y} and ${request.query.y}`);
+});
+
 
 // start the server
 app.listen(3000)
