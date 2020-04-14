1e813edc6cf14de3d45f51da9490511843d26dec
Acelyn <Acelyn@users.noreply.github.com>
2020-04-14 01:15:43 -0400

    one

---
+++ catalog/controllers/sections.js
@@ -0,0 +1,10 @@
+const Section = require('../models/section');
+
+// GET /sections?sort=
+module.exports.index = function(request, response, next) {
+  const order = request.query.sort || 'course'; // Default to sort by course
+
+  Section.find().sort(order)
+    .then(sections => response.render('sections/index', {sections: sections, order: order}))
+    .catch(error => next(error));
+};
